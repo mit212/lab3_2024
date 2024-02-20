108bb2c38f4f92112f3eac19cbee82c47f0c62ef
Josh Sohn <jcsohn@mit.edu>
2024-02-19 22:05:33 -0500

    pinout

--- include/pinout.h
+++ include/pinout.h
@@ -4,10 +4,14 @@
 //motor pins
 #define DIR1 39
 #define PWM1 40
+#define DIR2 41
+#define PWM2 42
 
 //encoder pins
-#define ENCODER_A_PIN 15
-#define ENCODER_B_PIN 16
+#define ENCODER1_A_PIN 21
+#define ENCODER1_B_PIN 5
+#define ENCODER2_A_PIN 15
+#define ENCODER2_B_PIN 16
 
 #define XPIN 1
 #define YPIN 2
