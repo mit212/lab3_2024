25ebbb9d9fe80ec31de8ec0e3ea94c99923aac56
jingerchong <jingersiachong@gmail.com>
2024-02-20 20:53:47 -0500

    remove serial from potentiometer

--- src/lab_code/potentiometer.cpp
+++ src/lab_code/potentiometer.cpp
@@ -2,8 +2,6 @@
 #include "pinout.h"
 
 void setup() {
-    Serial.begin();
-
     // TODO: Declare the potentiometer pins as INPUT. This should only take two lines.
     // Refer to `test_code/joystick_test.cpp` if needed.
 }
@@ -11,6 +9,4 @@ void setup() {
 void loop() {
     // TODO: Use analogRead to get the potentiometer inputs and update the output struct.
     // Refer to `test_code/joystick_test.cpp` if needed.
-
-    delay(100);
 }
